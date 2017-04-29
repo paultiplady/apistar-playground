2aea639d41435ad39e48151dbb0f0439f89fa182
Paul Tiplady <paultiplady@gmail.com>
2017-04-28 20:47:31 -0700

    Add a DB model

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from apistar import App, Route
+from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.declarative.api import declarative_base
+from sqlalchemy.sql.schema import Column
 
 
 def welcome():
@@ -10,3 +14,19 @@ routes = [
 ]
 
 app = App(routes=routes)
+
+Base = declarative_base()
+
+
+class Beer(Base):
+    __tablename__ = 'Beer'
+    id = Column(UUID, primary_key=True)
+    name = Column(String())
+
+
+settings = {
+    "DATABASE": {
+        "URL": "postgresql://paul:paul@localhost/apistar",
+        "METADATA": Base.metadata,
+    }
+}
